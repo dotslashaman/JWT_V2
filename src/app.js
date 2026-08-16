3cda1aa51095bf145a21131dcd2285f7e3382a86
Aman <meamanx7@gmail.com>
2026-08-16 21:22:08 +0530

    login router in progress

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const signUpRouter = require('./signup');
+const loginRouter = require('./login');
 app.use(express.json());
 
 app.get('/pingg', (req,res) => {
@@ -8,6 +9,10 @@ app.get('/pingg', (req,res) => {
 });
 
 app.use('/', signUpRouter);
+
+app.use ('/', loginRouter);
+
+
 
 
 
---
+++ src/login.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+require('dotenv').config();
+const mongoose = require("mongoose");
+const validate = require("./validations");
+const mongoCred = process.env.mongoUrl;
+
+
+mongoose.connect(mongoCred)
+.then(() => {
+    console.log("Database connected");
+})
+
+.catch((error) => {
+    console.log(error);
+})
+
+const userSchema = new mongoose.Schema({
+    email : String,
+    password : String,
+    age : Number
+});
+
+const db = mongoose.model('UserData', userSchema);
+
+router.post('/login', validate.validateEmail, validate.validatePassword, validate.validateAge, async(req,res) => {
+    
+})
